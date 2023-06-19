13c2535236cc9c3bbe4d6ad82f7766154f0326a6
lms8262 <zergling8262@gmail.com>
2023-06-19 17:23:40 +0900

    6.19 17:23

--- board2/src/main/java/com/example/board2/service/BoardService.java
+++ board2/src/main/java/com/example/board2/service/BoardService.java
@@ -1,9 +1,15 @@
 package com.example.board2.service;
 
+import java.util.List;
+
 import com.example.board2.dto.Board;
 
 public interface BoardService {
 	public int maxNum() throws Exception;
 
 	public void insertData(Board board) throws Exception;
+
+	public int getDataCount(String searchKey, String searchValue) throws Exception;
+
+	public List<Board> getLists(String searchKey, String searchValue, int start, int end) throws Exception;
 }
--- board2/src/main/java/com/example/board2/service/BoardServiceImpl.java
+++ board2/src/main/java/com/example/board2/service/BoardServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.board2.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,16 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	public void insertData(Board board) throws Exception {
 		boardMapper.insertData(board);
+	}
+
+	@Override
+	public int getDataCount(String searchKey, String searchValue) throws Exception {
+		return boardMapper.getDataCount(searchKey, searchValue);
+	}
+
+	@Override
+	public List<Board> getLists(String searchKey, String searchValue, int start, int end) throws Exception {
+		return boardMapper.getLists(searchKey, searchValue, start, end);
 	}
 
 }
