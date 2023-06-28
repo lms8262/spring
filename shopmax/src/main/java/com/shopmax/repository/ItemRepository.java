72bcf200ba180c13b2d1b4ee79765ec538d7041b
lms8262 <zergling8262@gmail.com>
2023-06-28 18:18:16 +0900

    6.28 18:18

--- shopmax/src/main/java/com/shopmax/repository/ItemRepository.java
+++ shopmax/src/main/java/com/shopmax/repository/ItemRepository.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shopmax.constant.ItemSellStatus;
 import com.shopmax.entity.Item;
@@ -26,4 +28,19 @@ public interface ItemRepository extends JpaRepository<Item, Long> { // 해당 re
 	List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 
 	List<Item> findByPriceLessThanOrderByPriceDesc(int price);
+	
+	//JPQL(non native 쿼리) -> 컬럼명, 테이블명은 반드시 entity 클래스 기준으로 작성한다.
+	@Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+	List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+	
+	//JPQL(native 쿼리) -> h2 데이터베이스를 기준으로 한 쿼리문 작성
+	@Query(value = "select * from item where item_detail like %:itemDetail% order by price desc", nativeQuery = true)
+	List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+	
+	@Query("select i from Item i where i.price >= :price")
+	List<Item> findByPriceGreaterThanEqual(@Param("price") int price);
+	
+	@Query("select i from Item i where i.itemNm = :itemNm and i.itemSellStatus = :itemSellStatus")
+	List<Item> findByItemNmAndItemSellStatus2(@Param("itemNm") String itemNm, @Param("itemSellStatus") ItemSellStatus itemSellStatus);
+	
 }
--- shopmax/src/test/java/com/shopmax/repository/ItemRepositoryTest.java
+++ shopmax/src/test/java/com/shopmax/repository/ItemRepositoryTest.java
@@ -149,4 +149,47 @@ class ItemRepositoryTest {
 			System.out.println(item.toString());
 		}
 	}
+	
+	@Disabled
+	@Test
+	@DisplayName("@Query를 이용한 상품 조회 테스트")
+	public void findByItemDetailTest() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세");
+		for(Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("@Query(native 쿼리)를 이용한 상품 조회 테스트")
+	public void findByItemDetailByNativeTest() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세");
+		for(Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("퀴즈2-1")
+	public void findByPriceGreaterThanEqualTest() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByPriceGreaterThanEqual(10005);
+		for(Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+	
+	@Test
+	@DisplayName("퀴즈2-2")
+	public void findByItemNmAndItemSellStatus2Test() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemNmAndItemSellStatus2("테스트 상품1", ItemSellStatus.SELL);
+		for(Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
 }
